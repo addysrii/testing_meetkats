ddcf9ad6f2b00d59f4a6d7d40ec13cb10f8f2378
addysrii <149177246+addysrii@users.noreply.github.com>
2025-06-22 21:56:10 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -13,7 +13,7 @@ import axios from 'axios';
 // };
 
 // const API_URL = getApiUrl();
-const API_URL ='https://new-backend-w86d.onrender.com';
+const API_URL =import.meta.env.VITE_API_BASE_URL;
 console.log(`API URL: ${API_URL}`);
 
 // Axios instance
